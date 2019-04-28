0d0e03aac08c701e40eac4cb71d943cf2dbd19f4
Justin Marciszewski <justin@harborislandanalytics.com>
2019-04-28 16:52:36 -0400

    generate Google Cloud AutoML Tables API package

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+# NEWS
+
+## 2019-04-28
+Generated Google Cloud [AutoML Tables](https://cloud.google.com/automl-tables/docs/apis) as [](/googleautomlv1beta1.auto)
---
+++ scripts/create_package.R
@@ -0,0 +1,14 @@
+# create package using googleAuthR auto discovery
+# https://github.com/MarkEdmondson1234/googleAuthR
+library(googleAuthR)
+
+# Discovery url for AutoML Tables:
+# https://automl.googleapis.com/$discovery/rest?version=v1beta1
+api_json_list <- gar_discovery_api("automl","v1beta1")
+
+directory <- getwd()
+
+result <- gar_create_package(api_json = api_json_list,
+                             directory = directory,
+                             github = FALSE,
+                             overwrite = FALSE)
