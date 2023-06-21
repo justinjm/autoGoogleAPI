42120fc875f9e799913f39f3d5ac88d8ddd112a5
Justin Marciszewski <justinjm@google.com>
2023-06-21 08:50:28 -0400

    initial commit - GCP Identity Platform
    https://cloud.google.com/identity-platform/docs/reference/rest

--- scripts/create_package.R
+++ scripts/create_package.R
@@ -1,10 +1,11 @@
 # create package using googleAuthR auto discovery
 # https://github.com/MarkEdmondson1234/googleAuthR
+library(here)
 library(googleAuthR)
 
-api_json_list <- gar_discovery_api("ml","v1")
+api_json_list <- gar_discovery_api("identitytoolkit","v2")
 
-directory <- getwd()
+directory <- here()
 
 result <- gar_create_package(api_json = api_json_list,
                              directory = directory,
